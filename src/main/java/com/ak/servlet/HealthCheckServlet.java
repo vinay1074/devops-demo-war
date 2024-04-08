c30a8a486f9609d7ce00392e02e902b449a5dc84
Yuvaraj K <yuvaraj.vinay74@gmail.com>
2024-04-08 18:23:14 +0530

    modify

---
+++ src/main/java/com/ak/servlet/HealthCheckServlet.java
@@ -0,0 +1,16 @@
+package com.ak.servlet;
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/health")
+public class HealthCheckServlet extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        response.setStatus(HttpServletResponse.SC_OK);
+        response.getWriter().println("{\"status\": \"ok\"}");
+    }
+}
